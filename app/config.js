70fadd1cb9377c2590d315b6efea12a1dce516a6
Yovanny Diaz <thebigyovadiaz@gmail.com>
2019-06-12 02:24:38 -0400

    Modified Files

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
-const port = process.env.PORT || 5001;
+const port = process.env.NODE_ENV || 5001;
 const key_dark_api = '0560b81fee6e7c7890f5c4c081336fab';
-const portRedis = 6379;
+const portRedis = process.env.REDISCLOUD_URL || 6379;
 
 module.exports = {
   port,
